67f85484eb3643e5765e16ba30c3ccc5659176d8
xywen97 <xywen22@cse.cuhk.edu.hk>
2026-01-13 13:34:05 +0800

    update files

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 python MMAgent/main.py \
     --model_name gpt-4o-mini \
-    --key sk-zk298f767a7a34739457e8df6f3afd50e5869417dab53465 \
-    --base_url https://api.zhizengzeng.com/v1 \
+    --key sk-xxx \
+    --base_url https://xxx/v1 \
     --task 2024_C
